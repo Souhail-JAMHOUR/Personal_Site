a4101b80478388cccebf3793a70c42eb93c541a0
Souhail <souhail.edu@gmail.com>
2022-10-25 18:42:01 +0100

    slash

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,7 @@ module.exports = {
     title: 'Souhail Jamhour',
     description:
       'Souhail is a full stack developper (and a music producer ) based in Oujda,Morocco',
-    siteUrl: 'https://souhailjamhour.netlify.app/', // No trailing slash allowed!
+    siteUrl: 'https://souhailjamhour.netlify.app', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
     twitterUsername: '',
   },
