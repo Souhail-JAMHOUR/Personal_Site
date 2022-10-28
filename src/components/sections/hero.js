b7e96511e62283b7e20b90cb29b694bb57a49352
Souhail <souhail.edu@gmail.com>
2022-10-28 23:06:59 +0100

    ldkf

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -60,7 +60,7 @@ const Hero = () => {
 
   const one = <h1>Hi, my name is</h1>;
   const two = <h2 className="big-heading">Souhail Jamhour.</h2>;
-  const three = <h3 className="big-heading">Student and Music Producer.</h3>;
+  const three = <h3 className="big-heading">Software Engineering Student</h3>;
   const four = (
     <>
       <p>
